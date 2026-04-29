7cf78123c9d08bb23741c14976e35727b1b4b0c0
Abdulrahim <abdelrahimatteib7@gmail.com>
2026-04-29 10:55:47 +0200

    Restore message_types.hpp with all 11 message types

--- message_types.hpp
+++ message_types.hpp
@@ -1,11 +1,17 @@
-#pragma once   // include guard
+#pragma once
 
 #include <cstdint>
 
 enum class MessageType : uint8_t {
-    Heartbeat = 1,
-    ClientHandshake = 2,
+    Heartbeat             = 1,
+    ClientHandshake       = 2,
     ClientAcknowledgement = 3,
-    ClientCommand = 4,
-    ClientReturnValue = 5
+    ClientCommand         = 4,
+    ClientReturnValue     = 5,
+    RemoteCommand         = 6,
+    RemoteReturnValue     = 7,
+    RemoteKill            = 8,
+    JobFinished           = 9,
+    RemoteJobFinished     = 10,
+    NodeDead              = 11
 };
